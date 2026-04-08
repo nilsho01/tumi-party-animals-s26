44ab1efaa5cbd5e38c30c866adcfc536cab12c49
Kristin <kristin.fratzke@tum.com>
2026-04-08 23:29:03 +0200

    Update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://pa-s26-api-8kup.onrender.com:5001";
+axios.defaults.baseURL = "https://pa-s26-api-8kup.onrender.com";
 // axios.defaults.baseURL = "http://localhost:5001";
 
 const Ctx = createContext(null);
